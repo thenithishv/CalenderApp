3f6a86ea4d55d5addde1904b1ee95d0bcf87bb9b
Nithish K S <thenithishv@gmail.com>
2024-12-31 00:54:54 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage'; // Adjust the path as necessary
-
+import AdminPage from './pages/AdminPage'; // Import other pages as necessary
+import UserPage from './pages/UserPage'; // Import other pages as necessary
 
 const App = () => {
     return (
-        <Router>
+        <Router basename="/CalenderApp"> {/* Set the base URL for routing */}
             <Routes>
-                <Route path="/" element={<HomePage />} />               {/* Add other routes here */}
+                <Route path="/" element={<HomePage />} />
+                <Route path="/admin" element={<AdminPage />} /> {/* Add other routes here */}
+                <Route path="/user" element={<UserPage />} />
             </Routes>
         </Router>
     );
 };
 
-export default App; 
+export default App;
